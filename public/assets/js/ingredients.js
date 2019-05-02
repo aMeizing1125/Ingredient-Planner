0396b0c43fdbec85da46192b417770d0dab9732e
unknown <hampton.muriel@gmail.com>
2019-05-01 22:16:31 -0400

    firebase after tutorial. it doesn't work tons of errors. Future Mei can deal with that.

--- public/assets/js/ingredients.js
+++ public/assets/js/ingredients.js
@@ -31,3 +31,66 @@ var uiConfig = {
 
 
 ui.start('#firebaseui-auth-container', uiConfig);
+
+//this works if any user tries to login or logout. 
+//because the onAuthStateChaned means both login or logout. 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    //you can switch style.display to initial if it doesn't break the style
+    //do it for both if user login or logout
+    document.getElementById('loginDiv').style.display = "block";
+    document.getElementById('logoutDiv').style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+      
+      var email_id = user.email; 
+
+      document.getElementById('logoutP').innerHTML = "Welcome Foodie : " + email;
+
+    }
+
+
+  } else {
+    // No user is signed in.
+    document.getElementById('loginDiv').style.display = "none";
+    document.getElementById('logoutDiv').style.display = "block";
+
+  }
+}); //end of firebase.auth()
+
+function login() {
+  // this is here to test the function only
+  // window.alert("login() working! ");
+
+  var userEmail = document.getElementById('emailInput').value;
+  var userPassword = document.getElementById('passwordInput').value;
+
+  //testing if userEmail and userPassword is actually capturing. 
+  // window.alert(userEmail + " " + userPassword);
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert('Error : ' + errorMessage);
+    // ...
+  });
+} //end of login()
+
+function logout() {
+  firebase.auth().signOut()
+
+  // tutorial deleted -commenting out is better
+  // .then(function() {
+  //   // Sign-out successful.
+  // }).catch(function(error) {
+  //   // An error happened.
+  // });
+
+
+
+}
