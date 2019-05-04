a43c591aca9edfc709073b4faace2babee3c7c3c
tyler <tylercedge@gmail.com>
2019-05-03 21:43:20 -0400

    was trying to get sequelize up i ended up switching to a seperate repo to sandbox that aspect https://github.com/TylerCEdge/projsandbox again this is for sequelize purposes so i dont have to tamper with other files for now

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the ingredients_DB if it exists currently --
+DROP DATABASE IF EXISTS ingredients_DB;
+-- Creates the "ingredients_DB" database --
+CREATE DATABASE ingredients_DB;
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
+\// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 
-var PORT = process.env.PORT || 3000;
-
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Requiring our models for syncing
+var db = require("./models");
 
-// Parse application body as JSON
-//why is it not recognizing middleware
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Static directory
+app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Routes
+// =============================================================
+require("./controllers/ingredients_controller.js")(app);
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/ingredients_controller.js");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
